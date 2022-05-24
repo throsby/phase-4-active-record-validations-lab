2eaf68bf88c98490a6ffe3e914030e34335145bb
Throsby Wells <throsbywells@gmail.com>
2022-05-24 10:16:17 -0400

    commit tree d0686c1f69389e82457521bb439c0506f425a0b1

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, { :length => { minimum: 250 } }
+    validates :summary, { :length => { maximum: 250 } }
+    validates :category, inclusion: { :in => ["Fiction", "Non-Fiction"] }
+    # validate :clickbaity
+    
+
+    # def clickbaity
+    #         errors.add(:title, "Must be more clickbait") unless self.title.include? "Won't Believe" or self.title.include? "Secret" or self.title.include? "Top" or self.title.include? "Guess"
+    # end
+
 end
